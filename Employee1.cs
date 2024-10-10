cfc9aa09860f6313145ff779f7a7bd04db6be460
dubeysourabh <103551362+dubeysourabh@users.noreply.github.com>
2024-10-10 12:48:43 +0530

    Add files via upload

---
+++ Employee1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Universal_Task.Models
+{
+    public class Employee1
+    {
+        public int EmpId { get; set; }
+        public string UserName { get; set; }
+        public string Address { get; set; }     
+        public string City { get; set; }
+        public string State { get; set; }
+        public DateTime? DoB { get; set; }
+        public string UniqueCode { get; set; }
+        public DateTime? Date { get; set; }
+        public byte[] Image { get; set; } // Store the image as byte array
+        public string Password { get; set; }
+    }
+}
